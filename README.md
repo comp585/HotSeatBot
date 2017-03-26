b45b58c90038e06027f61cd15e7bb29f0e39a4b0
dongy7 <dongy7@gmail.com>
2017-03-26 14:14:23 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hotseat
+
+Facebook Messenger bot for playing a game of truth or lie.
+
+## Dev
+
+This is a Node web app deployed to Heroku. Install the [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli)
+for deploying the app.
+
+### Deploying
+
+1. Commit local changes
+2. Push changes with Heroku CLI: `git push heroku master`
+
+
+## References
+
+- [FB Messenger Documentation](https://developers.facebook.com/docs/messenger-platform/guides/quick-start)
--- src/app.js
+++ src/app.js
@@ -33,10 +33,13 @@ app.listen(app.get("port"), function() {
   console.log("running on port", app.get("port"));
 });
 
+// webhook endpoint
 app.post("/webhook/", function(req, res) {
   let messaging_events = req.body.entry[0].messaging;
+
+  // iterate over the messages in batch
   for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i];
+    let event = messaging_events[i];
     let sender = event.sender.id;
     if (event.message && event.message.text) {
       let text = event.message.text;
@@ -46,6 +49,7 @@ app.post("/webhook/", function(req, res) {
   res.sendStatus(200);
 });
 
+// sends text to the sender
 function sendTextMessage(sender, text) {
   let messageData = { text: text };
   request(
